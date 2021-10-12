6b79c1ef637eb1378e5ef05931d96a45c65f93e4
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-10-12 18:21:42 -0300

    Update ytpy.py

--- ytpy.py
+++ ytpy.py
@@ -8,6 +8,7 @@ try:
     from colorama import Fore, Style
     vm_ex, vd_ex, cy_ex, a_ex, c_end = Fore.LIGHTRED_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTYELLOW_EX, Style.RESET_ALL
 except:
+    vm_ex, c_end = Fore.LIGHTRED_EX, Style.RESET_ALL
     print(f"{vm_ex}Pytube not installed\n Execute: pip3 install pytube{c_end}")
     raise SystemExit
 
@@ -20,19 +21,22 @@ def layout():
 \t\t   \__  |\___) ||_/ \__  |
 \t\t  (____/     |_|   (____/
 \n****************************************************************
-\n*     {c_end}By: {vm_ex}Amarau and ferreiraklet 2021{c_end}                         {cy_ex}*{c_end}
+\n*             {c_end}By: {vm_ex}Amarau and ferreiraklet 2021{c_end}                 {cy_ex}*{c_end}
 \n{cy_ex}*{c_end}  Github --> {vm_ex}github.com/Amarauu & github.com/ferreiraklet{c_end}     {cy_ex}*{c_end}
-\n{cy_ex}*{c_end}\t\t    {vm_ex}github.com/rodricbr{c_end}                        {cy_ex}*{c_end}
+\n{cy_ex}*{c_end}\t\t      {vm_ex}github.com/rodricbr{c_end}                      {cy_ex}*{c_end}
 \n{cy_ex}****************************************************************
 {c_end}""")
 
 
 def inputt():
     try:
-        ytvideo = str(input(f"{vd_ex}[+] Video's txt:{c_end} "))
+        ytvideo = str(input(f"{vm_ex}[+]{c_end} Video's txt: "))
+        if ytvideo == 'exit':
+            print(f'\n{vm_ex}Exiting... Bye!{c_end}')
+            raise SystemExit
         video(ytvideo)
     except KeyboardInterrupt:
-        print(f'\n{vm_ex}Exiting... Bye!{c_end}')
+        print(f'\n\n{vm_ex}Exiting... Bye!{c_end}')
 
 def video(video):
     videos_list = []
@@ -40,6 +44,7 @@ def video(video):
     downloading_video()
     with open(f"{os.getcwd()}/{video}", "r") as file:
         file_data = file.readlines()
+        start = time.time()
 
         for item in file_data:
             item = item.replace("\n", "")
@@ -57,8 +62,10 @@ def video(video):
             except Exception as e:
                 print(str(e))
             except KeyboardInterrupt:
-                print(f'\n{vm_ex}Exiting... Bye!{c_end}')
-    print(f"\n{vd_ex}Downloaded with success!\n{c_end}")
+                print(f'\n\n{vm_ex}Exiting... Bye!{c_end}')
+    end = time.time()
+    duration = end - start
+    print(f"\n{vd_ex}Downloaded with success!\nDuration:{c_end} {duration:.2f}\n")
     inputt()
 
 #print("Title:  ", youtube.title)
